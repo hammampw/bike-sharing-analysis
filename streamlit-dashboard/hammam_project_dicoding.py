c6d3612416b3de1d14f88571bcb25df5865c233f
Hammam Prasetyo W <160834341+hammampw@users.noreply.github.com>
2024-03-05 03:53:57 +0700

    Update hammam_project_dicoding.py

--- streamlit-dashboard/hammam_project_dicoding.py
+++ streamlit-dashboard/hammam_project_dicoding.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from babel.numbers import format_currency
 
 # Data Wrangling
 df_hari = pd.read_csv("streamlit-dashboard/day.csv")
